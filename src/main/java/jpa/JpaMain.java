d6f3e785802598f4ef763a78a42fa8a0ab38fc6e
cherish0-0 <jiyu01160@naver.com>
2025-06-19 14:09:52 +0900

    Initial commit
    - emf, em, Member 엔티티 생성 완료

---
+++ src/main/java/jpa/JpaMain.java
@@ -0,0 +1,15 @@
+package jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = emf.createEntityManager();
+
+        entityManager.close();
+        emf.close();
+    }
+}
